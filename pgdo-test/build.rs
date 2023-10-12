a8fa92f45f44f7e08b414bbf5a8203d38ba39906
Gavin Panella <gavin@allenap.me>
2023-10-12 23:10:33 +0200

    Simplify the `for_all_runtimes` macro

--- pgdo-test/build.rs
+++ pgdo-test/build.rs
@@ -1,45 +1,23 @@
 use std::error::Error;
-use std::path::PathBuf;
-
-use quote::quote;
 
 use pgdo::runtime::strategy::{Strategy, StrategyLike};
 
-type Result<T> = std::result::Result<T, Box<dyn Error>>;
-
-fn main() -> Result<()> {
-    let out_dir: PathBuf = std::env::var_os("OUT_DIR").expect("OUT_DIR not set").into();
-
+fn main() -> Result<(), Box<dyn Error>> {
     // The advice on this is not clear, but it seems that if we emit _any_
-    // `rerun-if-changed` directives, the default rules are not applied. This
-    // is, I think, the default.
+    // `rerun-if-changed` directives, the default rules are not applied. I think
+    // this approximates the default.
     println!("cargo:rerun-if-changed=.");
 
     // Use `pgdo` itself to discover runtimes.
     let strategy = Strategy::default();
 
-    // Since we cannot statically write out a `PathBuf` or OS string into Rust
-    // source, in order to interpolate them into the token stream we need to
-    // convert the `PathBuf`s we have into regular UTF-8 strings. This may fail.
-    let bindirs = strategy
-        .runtimes()
-        .map(|runtime| runtime.bindir.to_str().expect("invalid UTF-8").to_owned())
-        .collect::<Vec<_>>();
-
     // Ask Cargo to re-run if any of the runtime bindirs change. Sadly this will
     // not trigger when a new runtime is installed. I don't know how to do that
     // efficiently yet, especially on macOS w/ Homebrew because it would need to
     // watch the whole of the `Cellar` directory – which is a lot.
-    for bindir in bindirs.iter() {
-        println!("cargo:rerun-if-changed={bindir}");
+    for runtime in strategy.runtimes() {
+        println!("cargo:rerun-if-changed={}", runtime.bindir.display());
     }
-
-    let tokens = quote! { static RUNTIMES: &[&str] = &[#(#bindirs),*]; };
-    let syntax_tree = syn::parse2(tokens)?;
-    let formatted = prettyplease::unparse(&syntax_tree);
-
-    let runtimes_rs = out_dir.join("runtimes.rs");
-    std::fs::write(runtimes_rs, formatted)?;
 
     Ok(())
 }
--- pgdo-test/src/lib.rs
+++ pgdo-test/src/lib.rs
@@ -1,49 +1,46 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Ident, ItemFn, Signature};
 
-use pgdo::runtime::Runtime;
+use pgdo::runtime::strategy::{Strategy, StrategyLike};
 
-include!(concat!(env!("OUT_DIR"), "/runtimes.rs"));
-
+/// Attribute macro to generate a test function for each runtime discovered by
+/// `pgdo`. The function will be named after the original function, with a
+/// suffix containing the version number. The test body itself will have a
+/// "magic" `runtime` variable available, which is a [`pgdo::runtime::Runtime`].
 #[proc_macro_attribute]
 pub fn for_all_runtimes(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    // Parse the input tokens into a syntax tree
-    let input = parse_macro_input!(item as ItemFn);
-    // Extract parts of the input function
+    // Parse the input tokens into a syntax tree.
+    let input = syn::parse_macro_input!(item as syn::ItemFn);
+    // Extract parts of the input function.
     let vis = &input.vis;
     let block = &input.block;
 
-    let mut items = Vec::new();
+    Strategy::default()
+        .runtimes()
+        .map(|runtime| {
+            // Get the version of the runtime in an ident-friendlier format.
+            let version = runtime.version.to_string().replace('.', "_");
 
-    for bindir in RUNTIMES.iter() {
-        let runtime = Runtime::new(bindir).unwrap();
-        let version = runtime.version.to_string().replace('.', "_");
+            // Rename the function and change its signature.
+            let ident_with_version = syn::Ident::new(
+                format!("{ident}_v{version}", ident = input.sig.ident).as_str(),
+                input.sig.ident.span(),
+            );
+            let signature_with_version =
+                syn::Signature { ident: ident_with_version, ..input.sig.clone() };
 
-        // Rename the function and change its signature
-        let new_fn_name = Ident::new(
-            format!("{ident}_{version}", ident = input.sig.ident).as_str(),
-            input.sig.ident.span(),
-        );
-        let new_signature = Signature {
-            ident: new_fn_name,
-            // inputs: parse_quote!(runtime: ::pgdo::runtime::Runtime),
-            ..input.sig.clone()
-        };
+            // Generate the new function.
+            let bindir = runtime.bindir.to_str().unwrap();
+            let expanded = quote! {
+                #[::std::prelude::v1::test]
+                #vis #signature_with_version {
+                    let runtime = crate::runtime::Runtime::new(#bindir)?;
+                    #block
+                }
+            };
 
-        // Generate the new function
-        let expanded = quote! {
-            #[::std::prelude::v1::test]
-            #vis #new_signature {
-                let runtime = crate::runtime::Runtime::new(#bindir)?;
-                #block
-            }
-        };
-
-        // Return the generated tokens
-        let item = TokenStream::from(expanded);
-        items.push(item);
-    }
-
-    TokenStream::from_iter(items)
+            // Return the generated tokens.
+            TokenStream::from(expanded)
+        })
+        .collect::<TokenStream>()
 }
