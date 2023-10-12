f2c497f94a089032d7d1c8bbcbd90f7afb420422
Gavin Panella <gavin@allenap.me>
2023-10-12 22:46:42 +0200

    Use pgdo's `Strategy` to find runtimes
    Who knew this would work?

--- pgdo-test/build.rs
+++ pgdo-test/build.rs
@@ -3,68 +3,43 @@ use std::path::PathBuf;
 
 use quote::quote;
 
+use pgdo::runtime::strategy::{Strategy, StrategyLike};
+
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 fn main() -> Result<()> {
-    let out_dir: PathBuf = std::env::var_os("OUT_DIR")
-        .expect("OUT_DIR not set in build script")
-        .into();
+    let out_dir: PathBuf = std::env::var_os("OUT_DIR").expect("OUT_DIR not set").into();
 
-    let mut runtimes: Vec<PathBuf> = Vec::new();
+    // The advice on this is not clear, but it seems that if we emit _any_
+    // `rerun-if-changed` directives, the default rules are not applied. This
+    // is, I think, the default.
+    println!("cargo:rerun-if-changed=.");
 
-    // TODO: Replace code below with `pgdo::runtimes::strategy`-based logic.
+    // Use `pgdo` itself to discover runtimes.
+    let strategy = Strategy::default();
 
-    // Linux; Debian & Ubuntu.
-    //#[cfg(any(doc, target_os = "linux"))]
-    {
-        let glob: wax::Glob = "*/bin/pg_ctl".parse()?;
-        let roots = ["/usr/lib/postgresql".parse::<PathBuf>()?];
-        for root in roots.into_iter().filter(|path| path.is_dir()) {
-            for entry in glob.walk(root) {
-                if let Some(bindir) = entry?.into_path().parent() {
-                    println!("cargo:rerun-if-changed={}", bindir.display());
-                    runtimes.push(bindir.to_owned());
-                }
-            }
-        }
+    // Since we cannot statically write out a `PathBuf` or OS string into Rust
+    // source, in order to interpolate them into the token stream we need to
+    // convert the `PathBuf`s we have into regular UTF-8 strings. This may fail.
+    let bindirs = strategy
+        .runtimes()
+        .map(|runtime| runtime.bindir.to_str().expect("invalid UTF-8").to_owned())
+        .collect::<Vec<_>>();
+
+    // Ask Cargo to re-run if any of the runtime bindirs change. Sadly this will
+    // not trigger when a new runtime is installed. I don't know how to do that
+    // efficiently yet, especially on macOS w/ Homebrew because it would need to
+    // watch the whole of the `Cellar` directory – which is a lot.
+    for bindir in bindirs.iter() {
+        println!("cargo:rerun-if-changed={bindir}");
     }
 
-    // macOS; Homebrew.
-    #[cfg(any(doc, target_os = "macos"))]
-    {
-        let glob: wax::Glob = "postgresql@*/*/bin/pg_ctl".parse()?;
-        let roots = [
-            "/opt/homebrew/Cellar".parse::<PathBuf>()?,
-            "/usr/local/Cellar".parse()?,
-        ];
-        for root in roots.into_iter().filter(|path| path.is_dir()) {
-            for entry in glob.walk(root) {
-                if let Some(bindir) = entry?.into_path().parent() {
-                    println!("cargo:rerun-if-changed={}", bindir.display());
-                    runtimes.push(bindir.to_owned());
-                }
-            }
-        }
-    }
+    let tokens = quote! { static RUNTIMES: &[&str] = &[#(#bindirs),*]; };
+    let syntax_tree = syn::parse2(tokens)?;
+    let formatted = prettyplease::unparse(&syntax_tree);
 
-    // Write out `runtimes.rs`.
-    {
-        // Since we cannot statically write out a `PathBuf` or OS string into
-        // Rust source, in order to interpolate them into the token stream we
-        // need to convert the `PathBuf`s we have into regular UTF-8 strings.
-        // This may fail.
-        let runtimes = runtimes
-            .into_iter()
-            .map(|path| path.to_str().expect("invalid UTF-8").to_owned());
-        let tokens = quote! {
-            static RUNTIMES: &[&str] = &[#(#runtimes),*];
-        };
-        let syntax_tree = syn::parse2(tokens)?;
-        let formatted = prettyplease::unparse(&syntax_tree);
-
-        let runtimes_rs = out_dir.join("runtimes.rs");
-        std::fs::write(runtimes_rs, formatted)?;
-    }
+    let runtimes_rs = out_dir.join("runtimes.rs");
+    std::fs::write(runtimes_rs, formatted)?;
 
     Ok(())
 }
--- pgdo-test/src/lib.rs
+++ pgdo-test/src/lib.rs
@@ -2,13 +2,12 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, Ident, ItemFn, Signature};
 
+use pgdo::runtime::Runtime;
+
 include!(concat!(env!("OUT_DIR"), "/runtimes.rs"));
 
 #[proc_macro_attribute]
 pub fn for_all_runtimes(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    // println!("attr: {attr}");
-    // println!("item: {item}");
-
     // Parse the input tokens into a syntax tree
     let input = parse_macro_input!(item as ItemFn);
     // Extract parts of the input function
@@ -17,10 +16,13 @@ pub fn for_all_runtimes(_attr: TokenStream, item: TokenStream) -> TokenStream {
 
     let mut items = Vec::new();
 
-    for (num, runtime) in RUNTIMES.iter().enumerate() {
+    for bindir in RUNTIMES.iter() {
+        let runtime = Runtime::new(bindir).unwrap();
+        let version = runtime.version.to_string().replace('.', "_");
+
         // Rename the function and change its signature
         let new_fn_name = Ident::new(
-            format!("{ident}_{num}", ident = input.sig.ident).as_str(),
+            format!("{ident}_{version}", ident = input.sig.ident).as_str(),
             input.sig.ident.span(),
         );
         let new_signature = Signature {
@@ -33,7 +35,7 @@ pub fn for_all_runtimes(_attr: TokenStream, item: TokenStream) -> TokenStream {
         let expanded = quote! {
             #[::std::prelude::v1::test]
             #vis #new_signature {
-                let runtime = crate::runtime::Runtime::new(#runtime)?;
+                let runtime = crate::runtime::Runtime::new(#bindir)?;
                 #block
             }
         };
