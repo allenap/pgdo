3194843e3a1707e720ffcf38fc9601613aab3f35
Gavin Panella <gavin@allenap.me>
2023-10-16 19:39:41 +0200

    `ALTER SYSTEM` didn't exist before PostgreSQL 9.4

--- pgdo-lib/tests/test_cluster_config.rs
+++ pgdo-lib/tests/test_cluster_config.rs
@@ -3,7 +3,7 @@ use pgdo_test::for_all_runtimes;
 
 type TestResult = Result<(), ClusterError>;
 
-#[for_all_runtimes]
+#[for_all_runtimes(min = "9.4")]
 #[test]
 fn cluster_set_parameter() -> TestResult {
     let data_dir = tempdir::TempDir::new("data")?;
