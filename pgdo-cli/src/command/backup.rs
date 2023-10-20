775cb56d4293f04569924653013fdbe812d44851
Gavin Panella <gavin@allenap.me>
2023-10-20 19:28:56 +0200

    Make `with_cleanup` resilient to panics too

--- pgdo-cli/src/command/backup.rs
+++ pgdo-cli/src/command/backup.rs
@@ -191,17 +191,39 @@ where
     }
 }
 
-fn with_cleanup<T, E, F, FT, FE>(cleanup: F, task: impl FnOnce() -> Result<T, E>) -> Result<T, E>
+fn with_cleanup<TASK, T, E, CLEANUP, CT, CE>(cleanup: CLEANUP, task: TASK) -> Result<T, E>
 where
-    F: FnOnce() -> Result<FT, FE>,
-    FE: Into<E>,
+    TASK: std::panic::UnwindSafe + FnOnce() -> Result<T, E>,
+    CLEANUP: std::panic::UnwindSafe + FnOnce() -> Result<CT, CE>,
+    E: std::fmt::Display,
+    CE: std::fmt::Display + Into<E>,
 {
-    match task() {
-        Ok(ok) => Ok(ok),
-        Err(err) => {
-            cleanup().map_err(Into::into)?;
-            Err(err)
-        }
+    use std::panic::{catch_unwind, resume_unwind};
+
+    match catch_unwind(task) {
+        Ok(Ok(t)) => Ok(t),
+        Ok(Err(e)) => match catch_unwind(cleanup) {
+            Ok(Ok(_)) => Err(e),
+            Ok(Err(ce)) => {
+                log::error!("Task failed & cleaning-up also failed: {ce}");
+                Err(e)
+            }
+            Err(_) => {
+                log::error!("Task failed & cleaning-up panicked (suppressed)");
+                Err(e)
+            }
+        },
+        Err(panic) => match catch_unwind(cleanup) {
+            Ok(Ok(_)) => resume_unwind(panic),
+            Ok(Err(ce)) => {
+                log::error!("Task panicked & cleaning-up failed: {ce}");
+                resume_unwind(panic)
+            }
+            Err(_) => {
+                log::error!("Task panicked & cleaning-up also panicked (suppressed)");
+                resume_unwind(panic)
+            }
+        },
     }
 }
 
@@ -211,8 +233,10 @@ mod tests {
 
     #[test]
     fn test_with_cleanup() -> Result<(), Box<dyn std::error::Error>> {
-        let result: Result<&'static str, ()> =
-            with_cleanup(|| Ok::<&'static str, ()>("Ok/cleanup"), || Ok("Ok/task"));
+        let result: Result<&'static str, &'static str> = with_cleanup(
+            || Ok::<&'static str, &'static str>("Ok/cleanup"),
+            || Ok("Ok/task"),
+        );
         assert!(matches!(result, Ok("Ok/task")));
         Ok(())
     }
@@ -226,10 +250,27 @@ mod tests {
     }
 
     #[test]
+    #[should_panic(expected = "Panic/task")]
+    fn test_with_cleanup_panic_in_task() {
+        let _result: Result<(), &'static str> =
+            with_cleanup(|| Ok::<(), &'static str>(()), || panic!("Panic/task"));
+    }
+
+    #[test]
     fn test_with_cleanup_error_in_cleanup() -> Result<(), Box<dyn std::error::Error>> {
         let result: Result<(), &'static str> =
             with_cleanup(|| Err::<(), &'static str>("Err/cleanup"), || Ok(()));
-        assert!(matches!(result, Err("Err/cleanup")));
+        assert!(matches!(result, Ok(())));
+        Ok(())
+    }
+
+    #[test]
+    fn test_with_cleanup_panic_in_cleanup() -> Result<(), Box<dyn std::error::Error>> {
+        let result: Result<(), &'static str> = with_cleanup(
+            || -> Result<(), &'static str> { panic!("Panic/cleanup") },
+            || Ok(()),
+        );
+        assert!(matches!(result, Ok(())));
         Ok(())
     }
 
@@ -239,7 +280,16 @@ mod tests {
             || Err::<(), &'static str>("Err/cleanup"),
             || Err("Err/task")?,
         );
-        assert!(matches!(result, Err("Err/cleanup")));
+        assert!(matches!(result, Err("Err/task")));
         Ok(())
+    }
+
+    #[test]
+    #[should_panic(expected = "Panic/task")]
+    fn test_with_cleanup_panic_in_task_and_cleanup() {
+        let _result: Result<(), &'static str> = with_cleanup(
+            || -> Result<(), &'static str> { panic!("Panic/cleanup") },
+            || panic!("Panic/task"),
+        );
     }
 }
