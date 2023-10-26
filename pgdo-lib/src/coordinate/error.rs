9d996a046d0af2c27e1cdf766ec8550ee5a0ec87
Gavin Panella <gavin@allenap.me>
2023-10-26 22:39:28 +0200

    Convert other errors to use thiserror

--- pgdo-lib/src/coordinate/error.rs
+++ pgdo-lib/src/coordinate/error.rs
@@ -1,49 +1,13 @@
-use std::{error, fmt, io};
+use thiserror::Error;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum CoordinateError<C> {
-    IoError(io::Error),
-    UnixError(nix::Error),
+    #[error("input/output error: {0}")]
+    IoError(#[from] std::io::Error),
+    #[error("UNIX error: {0}")]
+    UnixError(#[from] nix::Error),
+    #[error(transparent)]
     ControlError(C),
+    #[error("cluster does not exist")]
     DoesNotExist,
-}
-
-impl<C> fmt::Display for CoordinateError<C>
-where
-    C: fmt::Display,
-{
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Self::IoError(ref e) => write!(fmt, "input/output error: {e}"),
-            Self::UnixError(ref e) => write!(fmt, "UNIX error: {e}"),
-            Self::ControlError(ref e) => e.fmt(fmt),
-            Self::DoesNotExist => write!(fmt, "cluster does not exist"),
-        }
-    }
-}
-
-impl<C> error::Error for CoordinateError<C>
-where
-    C: error::Error + 'static,
-{
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match *self {
-            Self::IoError(ref error) => Some(error),
-            Self::UnixError(ref error) => Some(error),
-            Self::ControlError(ref error) => Some(error),
-            Self::DoesNotExist => None,
-        }
-    }
-}
-
-impl<C> From<io::Error> for CoordinateError<C> {
-    fn from(error: io::Error) -> CoordinateError<C> {
-        Self::IoError(error)
-    }
-}
-
-impl<C> From<nix::Error> for CoordinateError<C> {
-    fn from(error: nix::Error) -> CoordinateError<C> {
-        Self::UnixError(error)
-    }
 }
--- pgdo-lib/src/runtime/error.rs
+++ pgdo-lib/src/runtime/error.rs
@@ -1,40 +1,11 @@
-use std::{error, fmt, io};
+use thiserror::Error;
 
 use crate::version;
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum RuntimeError {
-    IoError(io::Error),
-    VersionError(version::VersionError),
-}
-
-impl fmt::Display for RuntimeError {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        use RuntimeError::*;
-        match *self {
-            IoError(ref e) => write!(fmt, "input/output error: {e}"),
-            VersionError(ref e) => e.fmt(fmt),
-        }
-    }
-}
-
-impl error::Error for RuntimeError {
-    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
-        match *self {
-            RuntimeError::IoError(ref error) => Some(error),
-            RuntimeError::VersionError(ref error) => Some(error),
-        }
-    }
-}
-
-impl From<io::Error> for RuntimeError {
-    fn from(error: io::Error) -> RuntimeError {
-        RuntimeError::IoError(error)
-    }
-}
-
-impl From<version::VersionError> for RuntimeError {
-    fn from(error: version::VersionError) -> RuntimeError {
-        RuntimeError::VersionError(error)
-    }
+    #[error("input/output error: {0}")]
+    IoError(#[from] std::io::Error),
+    #[error(transparent)]
+    VersionError(#[from] version::VersionError),
 }
